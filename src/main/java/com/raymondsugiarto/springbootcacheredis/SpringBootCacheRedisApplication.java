dc1240288abbbcc488da5fd0a6533544dde17b7f
Raymond <114567325+raymondsugiarto-ap@users.noreply.github.com>
2023-02-26 19:13:10 +0700

    init project

---
+++ src/main/java/com/raymondsugiarto/springbootcacheredis/SpringBootCacheRedisApplication.java
@@ -0,0 +1,13 @@
+package com.raymondsugiarto.springbootcacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raymondsugiarto/springbootcacheredis/SpringBootCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymondsugiarto.springbootcacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
